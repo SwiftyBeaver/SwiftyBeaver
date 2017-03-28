65258ae18f6005d948b561a836d5e00e663cb6e7
Sebastian Kreutzberger <s.kreutzberger@googlemail.com>
2017-03-28 15:14:12 +0200

    added useXcode property to console

--- Sources/ConsoleDestination.swift
+++ Sources/ConsoleDestination.swift
@@ -11,7 +11,11 @@ import Foundation
 
 public class ConsoleDestination: BaseDestination {
 
+    /// use NSLog instead of print, default is false
     public var useNSLog = false
+    /// use colored output compatible with Xcode, default is true
+    /// if false then it uses colors compatible with Terminal
+    public var useXcode = true
 
     override public var defaultHashValue: Int { return 1 }
 
@@ -20,11 +24,25 @@ public class ConsoleDestination: BaseDestination {
 
         // use colored Emojis for better visual distinction
         // of log level for Xcode 8
-        levelColor.verbose = "💜 "     // silver
-        levelColor.debug = "💚 "        // green
-        levelColor.info = "💙 "         // blue
-        levelColor.warning = "💛 "     // yellow
-        levelColor.error = "❤️ "       // red
+        if useXcode {
+            // use colored Emojis for better visual distinction
+            // of log level for Xcode 8
+            levelColor.verbose = "💜 "     // silver
+            levelColor.debug = "💚 "        // green
+            levelColor.info = "💙 "         // blue
+            levelColor.warning = "💛 "     // yellow
+            levelColor.error = "❤️ "       // red
+
+        } else {
+            // use Terminal colors
+            reset = "\u{001b}[0m"
+            escape = "\u{001b}[38;5;"
+            levelColor.verbose = "251m"     // silver
+            levelColor.debug = "35m"        // green
+            levelColor.info = "38m"         // blue
+            levelColor.warning = "178m"     // yellow
+            levelColor.error = "197m"       // red
+        }
     }
 
     // print to Xcode Console. uses full base class functionality
