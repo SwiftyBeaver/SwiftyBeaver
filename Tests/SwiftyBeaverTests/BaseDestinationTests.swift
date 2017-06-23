ee121df920adfdad0da3541841ba917ebcc81103
Alessandro Orrù <alessandro.orru@moneyfarm.com>
2017-06-23 14:23:04 +0200

    Fixes validation of required filters

--- Tests/SwiftyBeaverTests/BaseDestinationTests.swift
+++ Tests/SwiftyBeaverTests/BaseDestinationTests.swift
@@ -322,6 +322,20 @@ class BaseDestinationTests: XCTestCase {
                                                        path: "/hello/foo.swift", function: "initialize"))
     }
 
+    func test_shouldLevelBeLogged_hasMinLevelAndOneRequiredMessageFilterAndDoesNotPass_False() {
+        let destination = BaseDestination()
+        destination.minLevel = .error
+        destination.addFilter(Filters.Message.contains("Required", caseSensitive: false, required: true, minLevel: .info))
+        XCTAssertFalse(destination.shouldLevelBeLogged(.info, path: "/hello/foo.swift", function: "initialize", message: "Test"))
+    }
+    
+    func test_shouldLevelBeLogged_hasMinLevelAndOneRequiredMessageFilterAndDoesPass_True() {
+        let destination = BaseDestination()
+        destination.minLevel = SwiftyBeaver.Level.error
+        destination.addFilter(Filters.Message.contains("Required", caseSensitive: false, required: true, minLevel: .info))
+        XCTAssertTrue(destination.shouldLevelBeLogged(.info, path: "/hello/foo.swift", function: "initialize", message: "Required Test"))
+    }
+    
     func test_shouldLevelBeLogged_hasLevelFilterAndTwoRequiredPathFiltersAndPasses_True() {
         let destination = BaseDestination()
         destination.minLevel = SwiftyBeaver.Level.info
