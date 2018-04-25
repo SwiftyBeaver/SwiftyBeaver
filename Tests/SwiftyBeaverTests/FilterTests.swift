e11fd3c556c986790687e8a36a92978876290c06
Nathan Harris <nathanh@l4digital.com>
2018-04-25 00:00:27 -0700

    add unit tests

--- Tests/SwiftyBeaverTests/FilterTests.swift
+++ Tests/SwiftyBeaverTests/FilterTests.swift
@@ -284,6 +284,31 @@ class FilterTests: XCTestCase {
         XCTAssertFalse(filter.apply("/first/path/to/anywhere"))
     }
 
+    func test_pathCustomSimple_answersTrue() {
+        let filter = Filters.Path.custom { string in
+            return string == "/Second/path/to/anywhere"
+        }
+        XCTAssertTrue(filter.apply("/Second/path/to/anywhere"))
+    }
+
+    func test_pathCustomComplexMatches_answersFalse() {
+        let filter = Filters.Path.custom { string in
+            let disallowedValues = ["/Second/path/to/anywhere"]
+            let allowedValues = ["/First/path/to/anywhere"]
+            return !disallowedValues.contains(string) && allowedValues.contains(string)
+        }
+        XCTAssertFalse(filter.apply("/Second/path/to/anywhere"))
+    }
+
+    func test_pathCustomComplexMatches_answersTrue() {
+        let filter = Filters.Path.custom { string in
+            let disallowedValues = ["/Second/path/to/anywhere"]
+            let allowedValues = ["/First/path/to/anywhere"]
+            return !disallowedValues.contains(string) && allowedValues.contains(string)
+        }
+        XCTAssertTrue(filter.apply("/First/path/to/anywhere"))
+    }
+
     //
     // Function filtering tests (identity)
     //
@@ -547,6 +572,31 @@ class FilterTests: XCTestCase {
     func test_functionEquals_hasMultipleValuesAndIsCaseSensitiveAndDoesNotMatch_answersFalse() {
         let filter = Filters.Function.equals("yourFunc", "myFunc", caseSensitive: true)
         XCTAssertFalse(filter.apply("myfunc"))
+    }
+
+    func test_functionCustomSimple_answersTrue() {
+        let filter = Filters.Function.custom { string in
+            return string == "myfunc"
+        }
+        XCTAssertTrue(filter.apply("myfunc"))
+    }
+
+    func test_functionCustomComplexMatches_answersTrue() {
+        let filter = Filters.Function.custom { string in
+            let disallowedValues = ["yourFunc", "yourOtherFunc"]
+            let allowedValues = ["myFunc"]
+            return !disallowedValues.contains(string) && allowedValues.contains(string)
+        }
+        XCTAssertTrue(filter.apply("myFunc"))
+    }
+
+    func test_functionCustomComplexMatches_answersFalse() {
+        let filter = Filters.Function.custom { string in
+            let disallowedValues = ["yourFunc", "yourOtherFunc"]
+            let allowedValues = ["myFunc"]
+            return !disallowedValues.contains(string) && allowedValues.contains(string)
+        }
+        XCTAssertFalse(filter.apply("yourFunc"))
     }
 
     //
@@ -822,6 +872,31 @@ class FilterTests: XCTestCase {
     func test_messageEquals_hasMultipleValuesAndIsCaseSensitiveAndDoesNotMatch_answersFalse() {
         let filter = Filters.Message.equals("goodbye, sluggishmink!", "hello there, swiftybeaver!", caseSensitive: true)
         XCTAssertFalse(filter.apply("Hello there, SwiftyBeaver!"))
+    }
+
+    func test_messageCustomSimple_answersTrue() {
+        let filter = Filters.Message.custom { string in
+            return string == "hello"
+        }
+        XCTAssertTrue(filter.apply("hello"))
+    }
+
+    func test_messageCustomComplexMatches_answersTrue() {
+        let filter = Filters.Message.custom { string in
+            let disallowedValues = ["goodbye", "see you later"]
+            let allowedValues = ["hello"]
+            return !disallowedValues.contains(string) && allowedValues.contains(string)
+        }
+        XCTAssertTrue(filter.apply("hello"))
+    }
+
+    func test_messageCustomComplexMatches_answersFalse() {
+        let filter = Filters.Function.custom { string in
+            let disallowedValues = ["goodbye", "see you later"]
+            let allowedValues = ["hello"]
+            return !disallowedValues.contains(string) && allowedValues.contains(string)
+        }
+        XCTAssertFalse(filter.apply("goodbye"))
     }
 
     // Helper functions
