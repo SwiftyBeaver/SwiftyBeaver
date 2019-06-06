787038dc3cd41ebb8b39b4341beae4b4c3b3000e
Laurent Gaches <laurent@binimo.com>
2019-06-06 10:45:41 +0200

    removed Xcode project and use Swift package manager instead

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let package = Package(
-    name: "SwiftyBeaver",
-    products: [
-        .library(name: "SwiftyBeaver", targets: ["SwiftyBeaver"])
-    ],
-    targets:[
-        .target(name: "SwiftyBeaver", dependencies: [], path: "Sources"),
-        .testTarget(name: "SwiftyBeaverTests", dependencies: ["SwiftyBeaver"])
-    ]
-)
+let package = Package(name: "SwiftyBeaver")
+
+package.platforms = [.macOS(.v10_12), .iOS(.v11), .tvOS(.v11), .watchOS(.v2)]
+package.products = [.library(name: "SwiftyBeaver", targets: ["SwiftyBeaver"])]
+package.swiftLanguageVersions = [.v5]
+package.targets = [
+    .target(name: "SwiftyBeaver", dependencies: [], path: "Sources"),
+    .testTarget(name: "SwiftyBeaverTests", dependencies: ["SwiftyBeaver"])
+]
