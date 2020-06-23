1e166bb751309e16cf5d5562041ccefc70181f71
Daniel Peter <daniel.peter@me.com>
2020-06-23 09:35:40 +0200

    Removes default platform deployment targets from Package.swift file

--- Package.swift
+++ Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyBeaver",
-    platforms: [
-        .macOS(.v10_10),
-        .iOS(.v8),
-        .tvOS(.v9),
-        .watchOS(.v2)
-    ],
     products: [
         .library(name: "SwiftyBeaver", targets: ["SwiftyBeaver"])
     ],
