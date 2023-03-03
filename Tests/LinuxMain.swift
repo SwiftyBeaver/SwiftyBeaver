0259cd50bd7364a039f54957b6a85bd573479a26
Sebastian Kreutzberger <s.kreutzberger@googlemail.com>
2023-03-03 09:50:51 +0100

    removed AES256CBC from Linux tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,28 +2,7 @@ import XCTest
 @testable import SwiftyBeaverTests
 
 XCTMain([
-    testCase(AES256CBCTests.allTests), // takes too long
     testCase(BaseDestinationTests.allTests),
-    //testCase(DestinationSetTests.allTests),
     testCase(ConsoleDestinationTests.allTests),
-    //testCase(FileDestinationTests.allTests),
-    //testCase(SBPlatformDestinationTests.allTests),
     testCase(SwiftyBeaverTests.allTests),
-    //testCase(GoogleCloudDestinationTests.allTests)
 ])
-
-// All tests:
-// the SBPlatformDestinationTests crashes testing under Linux with a linker issue?
-// Log into Docker container to find  issue:
-// docker run --rm -it -v $PWD:/app swiftybeaver /bin/bash -c "cd /app ; swift build ; swift test"
-
-/*
- 
- testCase(AES256CBCTests.allTests),
- testCase(BaseDestinationTests.allTests),
- testCase(DestinationSetTests.allTests),
- testCase(FileDestinationTests.allTests),
- testCase(SBPlatformDestinationTests.allTests),
- testCase(SwiftyBeaverTests.allTests),
- 
- */
